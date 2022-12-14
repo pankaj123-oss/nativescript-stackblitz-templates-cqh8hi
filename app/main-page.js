e42af60242215f983aa7ffeefd0217e19cb3c01f
pankaj123-oss <57318079+pankaj123-oss@users.noreply.github.com>
2022-12-14 14:04:35 +0530

    3 files modified, 1 added

--- app/main-page.js
+++ app/main-page.js
@@ -1,7 +1,13 @@
 import { createViewModel } from './main-view-model';
-
+import { Observable } from '@nativescript/core';
+export function onNavigatingTo(args) {
+  const vm = new Observable();
+  const page = args.object;
+  page.bindingContext = vm;
+}
 export function onTap(args) {
-  const button = args.object;
-  const page = button.page;
-  page.frame.navigate('home');
+  const vm = new Observable();
+  var name = vm.get('name');
+  var pass = vm.get('pass');
+  console.log(name);
 }
